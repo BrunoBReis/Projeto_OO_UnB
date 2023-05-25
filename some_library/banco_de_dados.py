bddf2a9c70d075a8beccdeaac20caa8a7b3a0ab2
Davi Levy <daviklevy@gmail.com>
2023-05-25 16:45:54 -0300

    Criei as funções "Remover_user" e "Editar_user"

--- some_library/banco_de_dados.py
+++ some_library/banco_de_dados.py
@@ -22,8 +22,8 @@ class DataBank_Historico():
         print (historico)
 
 
-#with open("Clientes.json") as file:
-#    clientes = json.load(file)
+with open("Clientes.json") as file:
+    clientes = json.load(file)
 
 def cadastrar_user(clientes, tipo, nome, endereco, telefone, senha, codigo):
     novo_cliente = {"Tipo" : tipo, "Nome" : nome, "Endereco" : endereco, "Telefone" : telefone, "Senha" : senha}
@@ -31,5 +31,33 @@ def cadastrar_user(clientes, tipo, nome, endereco, telefone, senha, codigo):
     with open("Clientes.json", "w") as arquivo:
         json.dump(clientes, arquivo, indent=4)
 
-clientes = {}
+def remover_user(clientes, codigo):
+    clientes.pop(codigo, "ok")
+    with open("Clientes.json", "w") as arquivo:
+        json.dump(clientes, arquivo, indent=4)
+
+def editar_user(clientes, codigo):
+    print ("""O que você quer editar?
+1)Nome
+2)Endereco
+3)Telefone
+4)Senha
+5)Sair do modo de edição""")
+    res = input("Resposta: ")
+    if res == "1":
+        clientes[codigo]["Nome"] = input("Novo Nome: ")
+    if res == "2":
+        clientes[codigo]["Endereco"] = input("Novo Endereco: ")
+    if res == "3":
+        clientes[codigo]["Telefone"] = input("Nova Telefone: ")
+    if res == "4":
+        clientes[codigo]["Senha"] = input("Nova Senha: ")
+    else:
+        pass
+    with open("Clientes.json", "w") as arquivo:
+        json.dump(clientes, arquivo, indent=4)
+
+
 cadastrar_user(clientes, "Pessoa", "Julia", "abcdef", "99122-3445", "abcd123", "100027")
+#remover_user(clientes, "100027")
+editar_user(clientes, "100027")
