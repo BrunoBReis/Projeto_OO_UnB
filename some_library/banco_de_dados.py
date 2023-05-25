9ba758ce394e9b133ad819db547ccfe26a2d4164
Davi Levy <daviklevy@gmail.com>
2023-05-25 17:00:29 -0300

    Criei a função "Visualiza_user"

--- some_library/banco_de_dados.py
+++ some_library/banco_de_dados.py
@@ -22,11 +22,8 @@ class DataBank_Historico():
         print (historico)
 
 
-with open("Clientes.json") as file:
-    clientes = json.load(file)
-
-def cadastrar_user(clientes, tipo, nome, endereco, telefone, senha, codigo):
-    novo_cliente = {"Tipo" : tipo, "Nome" : nome, "Endereco" : endereco, "Telefone" : telefone, "Senha" : senha}
+def cadastrar_user(clientes, tipo, nome, endereco, telefone, senha, codigo, cpf_cnpj):
+    novo_cliente = {"Tipo" : tipo, "Nome" : nome, "Endereco" : endereco, "Telefone" : telefone, "Senha" : senha, "CPF/CNPJ" : cpf_cnpj}
     clientes.update({codigo : novo_cliente})
     with open("Clientes.json", "w") as arquivo:
         json.dump(clientes, arquivo, indent=4)
@@ -57,7 +54,20 @@ def editar_user(clientes, codigo):
     with open("Clientes.json", "w") as arquivo:
         json.dump(clientes, arquivo, indent=4)
 
+def visualiza_user(clientes):
+    for conta in clientes:
+        print (f"[{conta}]\n\n")
+        for item in clientes[conta]:
+            print (f"{item}: {clientes[conta][item]}")
+        print ("\n\n")
 
-cadastrar_user(clientes, "Pessoa", "Julia", "abcdef", "99122-3445", "abcd123", "100027")
+
+with open("Clientes.json") as file:
+    clientes = json.load(file)
+cadastrar_user(clientes, "Pessoa", "Julia", "abcdef", "99122-3445", "abcd123", "100027", "73809566666")
+cadastrar_user(clientes, "Empresa", "Apple", "jbfahsdbjv", "8000-3420", "apple2000", "000184", "20-2394889898.38")
 #remover_user(clientes, "100027")
-editar_user(clientes, "100027")
+#editar_user(clientes, "100027")
+with open("Clientes.json") as file:
+    clientes = json.load(file)
+visualiza_user(clientes)
