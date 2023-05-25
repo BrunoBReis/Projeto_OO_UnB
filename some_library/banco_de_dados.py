7400ef416fc46138f9efd81b420c6f2f415d33b7
Davi Levy <daviklevy@gmail.com>
2023-05-25 16:21:43 -0300

    Atualizando para lidar com arquivos ".json"

--- some_library/banco_de_dados.py
+++ some_library/banco_de_dados.py
@@ -1,24 +1,35 @@
+import json
+
 class DataBank_Gerente():
 
-    def get_data (self, name = "Gerentes.txt"):
-        data = open(name, "r", encoding="utf-8")
-        gerentes = data.readlines()
+    def get_data (self, name = "Gerentes.json"):
+        with open(name) as arquivo:
+            gerentes = json.load(arquivo)
         print (gerentes)
-        data.close
-
 
 class DataBank_Clientes():
 
-    def get_data (self, name = "Clientes.txt"):
-        data = open(name, "r", encoding="utf-8")
-        clientes = data.readlines()
+    def get_data (self, name = "Clientes.json"):
+        with open(name) as arquivo:
+            clientes = json.load(arquivo)
         print (clientes)
-        data.close
 
 class DataBank_Historico():
 
-    def get_data (self, name = "Historico.txt"):
-        data = open(name, "r", encoding="utf-8")
-        historico = data.readlines()
+    def get_data (self, name = "Historico.json"):
+        with open(name) as arquivo:
+            historico = json.load(arquivo)
         print (historico)
-        data.close
+
+
+#with open("Clientes.json") as file:
+#    clientes = json.load(file)
+
+def cadastrar_user(clientes, tipo, nome, endereco, telefone, senha, codigo):
+    novo_cliente = {"Tipo" : tipo, "Nome" : nome, "Endereco" : endereco, "Telefone" : telefone, "Senha" : senha}
+    clientes.update({codigo : novo_cliente})
+    with open("Clientes.json", "w") as arquivo:
+        json.dump(clientes, arquivo, indent=4)
+
+clientes = {}
+cadastrar_user(clientes, "Pessoa", "Julia", "abcdef", "99122-3445", "abcd123", "100027")
