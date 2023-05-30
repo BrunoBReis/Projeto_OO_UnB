7cc5d76d56bf1f320e89b8532a92b6d16e2e90fe
Bruno Bragança dos Reis <62809606+BrunoBReis@users.noreply.github.com>
2023-05-30 12:56:48 -0300

    implementando uma função

--- some_library/bruno_teste.py
+++ some_library/bruno_teste.py
@@ -1,13 +1,56 @@
 import json
 
 class SistemaBancario:
+
+# Criando uma composição da classe "BancoDeDados" em "SistemaBancario"
+
     def __init__(self):
         self.bancoDados = BancoDeDados()
     
+# Função para reconhecer código da conta e senha para logar como "Gerente" ou "Cliente"
+
+    def login(self):
+        print("""Escolha uma das opções abaixo:
+1) Gerente
+2) Cliente
+
+Resposta: """)
+        esco = input(" ")
+        if esco  == "1":
+            print("\n\n GERENTE \n\n")
+            cod = input("Código da conta: ")
+            senha = input("Senha: ")
+            if (cod in self.bancoDados.gerentes) == True:
+                if (senha in self.bancoDados.gerentes[cod]["Senha"]) == True:
+                    self.bancoDados.ger = self.bancoDados.gerentes[cod]
+                    nome = self.bancoDados.ger["Nome"]
+                    print(f"Login com Sucesso! Seja bem vindo {nome}")
+                else:
+                    print("Falha no login! Senha incorreta!")
+            else: 
+                print("Falha no login, conta inexistente!")
+        else:
+            print("\n\n CLIENTE \n\n")
+            cod = input("Código da conta: ")
+            senha = input("Senha: ")
+            if (cod in self.bancoDados.clientes) == True:
+                if (senha in self.bancoDados.clientes[cod]["Senha"]) == True:
+                    self.bancoDados.cli = self.bancoDados.clientes[cod]
+                    nome = self.bancoDados.cli["Nome"]
+                    print(f"Login com Sucesso! Seja bem vindo {nome}")
+                else:
+                    print("Falha no login! Senha incorreta!")
+            else: 
+                print("Falha no login, conta inexistente!")
+        
+
     def menu(self):
         pass
 
 class BancoDeDados:
+
+# Função construtora para pegar a base de dados nos arquivos ".json" e tranformar em dict no codigo
+# "Self.hist", "self.cli" e "self.ger" são objetos temporários para receber o cliente recém-logado
 
     def __init__(self):
         with open("Gerentes.json") as GeFile:
@@ -16,6 +59,9 @@ class BancoDeDados:
             self.clientes = json.load(CliFile)
         with open("Historico.json") as HistFile:
             self.historico = json.load(HistFile)
+        self.hist = {}
+        self.cli = {}
+        self.ger = {}
 
     def armazena_dados(self):
         pass
@@ -23,7 +69,7 @@ class BancoDeDados:
     def busca_dados(self):
         pass
         
-
+# na classe usuário precisa-se colar um atributo de limite com um valor fixo de 1000
 class Usuario:
     def __init__(self, nome, endereco, telefone, senha, codigo):
         self.nome = nome
@@ -58,10 +104,10 @@ class Cliente(Usuario):
             self.saldo -= valor
             print(f'Você sacou {valor} reais da sua conta')
         else:
-            print(f'{self.nome}saldo insuficiente')
+            print(f'{self.nome} saldo insuficiente')
     
-    def depositar(self):
-        pass
+    def depositar(self, valor):
+        self.saldo += valor
 
     def pagamento_programado(self):
         pass
@@ -71,16 +117,16 @@ class Cliente(Usuario):
     
     
 class Empresa(Cliente):
-    def __init__(self, saldo, nome, endereco, telefone, senha, cnpj):
-        super().__init__(saldo, nome, endereco, telefone, senha)
+    def __init__(self, saldo, nome, endereco, telefone, senha, cnpj, codigo):
+        super().__init__(saldo, nome, endereco, telefone, senha, codigo)
         self.cnpj = cnpj
 
     def solicitar_credito(self):
         pass
     
 class PessoaFisica(Cliente):
-    def __init__(self, saldo, nome, endereco, telefone, senha, cpf):
-        super().__init__(saldo, nome, endereco, telefone, senha)
+    def __init__(self, saldo, nome, endereco, telefone, senha, cpf, codigo):
+        super().__init__(saldo, nome, endereco, telefone, senha, codigo)
         self.cpf = cpf
 
     def solicitar_credito(self):
