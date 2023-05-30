eb83f7c2f4b2751c073427bd555ae173814b3e72
Bruno Bragança dos Reis <62809606+BrunoBReis@users.noreply.github.com>
2023-05-30 11:10:49 -0300

    aplicado função sacar dentro do worksapce

--- some_library/bruno_main.py
+++ some_library/bruno_main.py
@@ -1,3 +1,9 @@
 from some_library.bruno_teste import *
 
-bruno = Cliente()
+def workspace():
+    bruno = Cliente(1000, 'Bruno', 'endereço', 'telefone', 'senha123', '432423423')
+    bruno.sacar(50)
+    print(bruno.saldo())
+
+if __name__ == '__main__':
+    workspace
