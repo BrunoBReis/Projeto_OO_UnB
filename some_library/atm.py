52a6836e7344e30caadf23eca9e71eecf06b83cc
BrunoBReis <brunobragancadosreis@gmail.com>
2023-06-15 09:48:11 -0300

    comentando todo o  código do atm e alterando pequenas coisas no main

--- some_library/atm.py
+++ some_library/atm.py
@@ -1,15 +1,13 @@
 import json
 import datetime
 
-#---------------------------------------------------------------------------------------
-#------------------CLASSE BANCO DE DADOS-------------------------------------------------------
-#---------------------------------------------------------------------------------------
 
 class BancoDeDados:
-
-# Função construtora para pegar a base de dados nos arquivos ".json" e tranformar em dict no codigo
+    """ Classe responsável por inicializar todos os bancos de dados """
 
     def __init__(self):
+        """ Inicializando os bancos de dados """
+
         with open("Gerentes.json") as GeFile:
             self.gerentes = json.load(GeFile)
         with open("Clientes.json") as CliFile:
@@ -22,14 +20,12 @@ class BancoDeDados:
             self.atualizacoes = json.load(Atufile)
 
 
-#---------------------------------------------------------------------------------------
-#------------------CLASSE USUÁRIO-------------------------------------------------------
-#---------------------------------------------------------------------------------------
-
-# na classe usuário precisa-se colar um atributo de limite com um valor fixo de 1000
 class Usuario:
+    """ Classe responsável por definir o usuário """
     
     def __init__(self, nome, endereco, telefone, senha, codigo, tipo):
+        """ Inicializando os atributos do usuário """
+
         self.nome = nome
         self.endereco = endereco
         self.telefone = telefone
@@ -37,20 +33,30 @@ class Usuario:
         self.codigo = codigo
         self.tipo = tipo
 
-#---------------------------------------------------------------------------------------
-#------------------CLASSE GERENTE-------------------------------------------------------
-#---------------------------------------------------------------------------------------
 
 class Gerente(Usuario):
+    """ Classe responsável pelas características de gerente herdando o usuário"""
     
     def __init__(self, nome, endereco, telefone, senha, codigo, tipo):
+        """ Inicializando os atributos """
+
         super().__init__(nome, endereco, telefone, senha, codigo, tipo)
     
     def cadastrar_user(self, clientes, historico, programado, tipo, nome, endereco, telefone, senha, codigo, cpf_cnpj, saldo):
-        novo_cliente = {"Tipo" : tipo, "Nome" : nome, "Endereco" : endereco, "Telefone" : telefone, "Senha" : senha, "CPF/CNPJ" : cpf_cnpj, "Saldo" : saldo, "Credito" : 0.0}
+        """ Cadastramento do cliente pelo gerente e adicionado no banco de dados Clientes, Historico e Pagamento_programdo """
+  
+        novo_cliente = {"Tipo" : tipo,
+                        "Nome" : nome,
+                        "Endereco" : endereco,
+                        "Telefone" : telefone,
+                        "Senha" : senha,
+                        "CPF/CNPJ" : cpf_cnpj,
+                        "Saldo" : saldo,
+                        "Credito" : 0.0,}
         clientes.update({codigo : novo_cliente})
         historico.update({codigo : {}})
         programado.update({codigo : {}})
+
         with open("Clientes.json", "w") as arquivo:
             json.dump(clientes, arquivo, indent=4)
         with open("Historico.json", "w") as arquivo:
@@ -59,16 +65,24 @@ class Gerente(Usuario):
             json.dump(programado, arquivo, indent=4)
     
     def remover_user(self, clientes, codigo):
+        """ Remossão do cliente pelo gerente e adicionado ao banco de dados Clientes """
+
         clientes.pop(codigo, "Cliente não existe")
+
         with open("Clientes.json", "w") as arquivo:
             json.dump(clientes, arquivo, indent=4)
     
     def editar_user(self, clientes, codigo, dado, novo_dado):
+        """ Edição de usuários pelo cliente e adicionado ao banco de dados Clientes """
+
         clientes[codigo][dado] = novo_dado
+
         with open("Clientes.json", "w") as arquivo:
             json.dump(clientes, arquivo, indent=4)
         
     def visualiza_user(self, clientes):
+        """ Visualização do cliente pelo gerente """
+
         for conta in clientes:
             print (f"[{conta}]\n\n")
             for item in clientes[conta]:
@@ -76,39 +90,48 @@ class Gerente(Usuario):
             print ("\n\n")
 
 
-#---------------------------------------------------------------------------------------
-#------------------CLASSE CLIENTE-------------------------------------------------------
-#---------------------------------------------------------------------------------------
-
 class Cliente(Usuario):
+    """ Classe responsável pelo cliente herdando as características de usuário """
+
     def __init__(self, saldo, nome, endereco, telefone, senha, codigo, tipo):
+        """ Inicializando a classe com as características de usuário mais o saldo """
+
         super().__init__(nome, endereco, telefone, senha, codigo, tipo)
         self.saldo = saldo
 
     def sacar(self, valor, clientes, codigo, saldo):
+        """ Saque feito pelo cliente e adicioando ao banco de dados Clientes e registrado pela função registrar_transacao"""
+
         saldo -= valor
         clientes[codigo]["Saldo"] = saldo
+        
         with open('Clientes.json', 'w') as clientes_file:
             json.dump(clientes, clientes_file, indent=4)
+        
         self.registrar_transacao(valor, codigo, 'Saque', saldo)
     
     def depositar(self, valor, clientes, codigo, saldo):
+        """ Deposito feito pelo cliente e adicionado ao banco de dados Clientes e registrado pela função registrar_transacao """
+
         clientes[codigo]['Saldo'] += valor
         saldo += valor
+        
         with open('Clientes.json', 'w') as clientes_file:
             json.dump(clientes, clientes_file, indent=4)
+        
         self.registrar_transacao(valor, codigo, 'Deposito', saldo)
         
-    # sugestão do professor foi fazer uma implentação disso no json como um pagamento
-    # que está agendado e posteriormente confirmar essa data com algum bibloteca 
-    # depois relizar o pagamento automaticamente
     def pagamento_programado(self, programado, dic, codigo, num):
+        """ Pagamento programdo pelo cliente e adicioando ao banco de dados Pagamento_programado """
+    
         programado[codigo].update({num : dic})
         
         with open('Pagamento_programado.json', 'w') as pagfile:
             json.dump(programado, pagfile, indent=4)
     
     def visualizar_historico(self):
+        """ Visualização do histórico pelo cliente """
+
         with open('Historico.json') as historico_file:
             historico_lista = json.load(historico_file)
 
@@ -118,6 +141,8 @@ class Cliente(Usuario):
             print('--------------------')
 
     def registrar_transacao(self, valor, codigo, tipo_transacao, saldo):
+        """ Registro da transação e adicioando ao banco de dados Historico """
+
         data_hoje = datetime.datetime.now()
         data_hoje_str = data_hoje.strftime("%d/%m/%Y")
         data_agora_str = data_hoje.strftime("%I:%M:%S")
@@ -135,52 +160,54 @@ class Cliente(Usuario):
         
         if (data_hoje_str in historico_dic[codigo]) == True:
             historico_dic[codigo][data_hoje_str].update({data_agora_str : transacao2})
-  
         else:
             historico_dic[codigo].update({data_hoje_str : transacao})
 
         with open('Historico.json', 'w') as historico_update:
             json.dump(historico_dic, historico_update, indent=4)
         
-
-    
-
-#---------------------------------------------------------------------------------------
-#------------------CLASSE EMPRESA-------------------------------------------------------
-#---------------------------------------------------------------------------------------
-
     
 class Empresa(Cliente):
+    """ Classe responsável pelas características da empresa herdando o cliente """
+    
     def __init__(self, saldo, nome, endereco, telefone, senha, cnpj, tipo, codigo):
+        """ Inicializando as características da empresa """
+
         super().__init__(saldo, nome, endereco, telefone, senha, tipo, codigo)
         self.cnpj = cnpj
 
-# a ideia de slocitar crédito partiria de utilizar o pagamento agendado e acrescentar um juros
-# para ser pago em uma certa data
     def solicitar_credito(self, valor, clientes, codigo, credito, saldo):
+        """ Soliciatação de crédito pela empresa e adicionado ao banco de dados Clientes e registando transação """
+
         credito += valor
         saldo += valor
         clientes[codigo]["Saldo"] = saldo
         clientes[codigo]["Credito"] = credito
+        
         with open('Clientes.json', 'w') as clientes_file:
             json.dump(clientes, clientes_file, indent=4)
+        
         self.registrar_transacao(valor, codigo, 'Credito', saldo)
     
 
-#---------------------------------------------------------------------------------------
-#------------------CLASSE PESSOA FÍSICA-------------------------------------------------------
-#---------------------------------------------------------------------------------------
-
 class PessoaFisica(Cliente):
+    """Classe responsável pela Pessoa física e herdando as características de cliente """
+
     def __init__(self, saldo, nome, endereco, telefone, senha, cpf, tipo, codigo):
+        """ Inicializando as características de pessoa física """
+
         super().__init__(saldo, nome, endereco, telefone, senha, tipo, codigo)
         self.cpf = cpf
 
     def solicitar_credito(self, valor, clientes, codigo, credito, saldo):
+        """ Solicitando crédito pela pessoa física e adicionando ao banco de dados Clientes e registrando transação """
+
         credito += valor
         saldo += valor
         clientes[codigo]["Saldo"] = saldo
         clientes[codigo]["Credito"] = credito
+
         with open('Clientes.json', 'w') as clientes_file:
             json.dump(clientes, clientes_file, indent=4)
+
         self.registrar_transacao(valor, codigo, 'Credito', saldo)
